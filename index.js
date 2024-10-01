3ebefb93c6855cb1b70dc07c63a8c775ccf46d00
dann <austindan168@gmail.com>
2024-10-01 12:37:22 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file!
+// // Code your solution in this file!
+
+// function distanceFromHqInBlocks(pickupLocation, scuberHeadquarters = 42){
+//    return pickupLocation - scuberHeadquarters
+// }
+// console.log(distanceFromHqInBlocks(50))
+// console.log(distanceFromHqInBlocks())
+
+const sum = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+  }
+  console.log(sum(1,2))
